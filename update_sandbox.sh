8c7b1fd39de29903994fcf0d33eaa078494097a1
lew <LGordon2@gmail.com>
2014-05-02 16:36:58 -0400

    Updated with real article content!

--- update_sandbox.sh
+++ update_sandbox.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp smooth-scroll.js test.css test.html admin@portalbox:/var/www/rural-directory/new_rr/
+scp smooth-scroll.js test.css test.html me.png admin@portalbox:/var/www/rural-directory/new_rr/
